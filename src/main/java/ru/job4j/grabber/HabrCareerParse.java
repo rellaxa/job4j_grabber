b1f125669de8ec40ccbea5dccdf9f7a0d382bcae
relaxa <ewg.nadeev@yandex.ru>
2024-09-12 19:18:55 +0300

    2.4. HabrCareerParse [#285213 #204193]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -10,16 +10,23 @@ import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringJoiner;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final int MAX_PAGE = 5;
     private static final String SOURCE_LINK = "https://career.habr.com";
-    public static final String PREFIX = "/vacancies?page=";
-    public static final String SUFFIX = "&q=Java%20developer&type=all";
+    private static final String PREFIX = "/vacancies?page=";
+    private static final String SUFFIX = "&q=Java%20developer&type=all";
+    private final DateTimeParser dateTimeParser;
 
-    private static String retrieveDescription(String link)  {
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    private static String retrieveDescription(String link) {
         Connection connection = Jsoup.connect(link);
         try {
             Document document = connection.get();
@@ -30,30 +37,45 @@ public class HabrCareerParse {
         }
     }
 
+    @Override
+    public List<Post> list(String link) throws IOException {
+        Connection connection = Jsoup.connect(link);
+        List<Post> posts = new ArrayList<>();
+
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+
+            Element titleElement = row.select(".vacancy-card__title").first();
+            Element linkElement = titleElement.child(0);
+            String vacancyName = titleElement.text();
+            String date = row.select(".vacancy-card__date")
+                    .first()
+                    .child(0)
+                    .attr("datetime");
+            LocalDateTime dateCreated = dateTimeParser.parse(date);
+            String vacancyLink = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            String description = retrieveDescription(vacancyLink);
+
+            Post post = new Post();
+            post.setTitle(vacancyName);
+            post.setLink(vacancyLink);
+            post.setDescription(description);
+            post.setCreated(dateCreated);
+            posts.add(post);
+        });
+        return posts;
+    }
+
     public static void main(String[] args) throws IOException {
         DateTimeParser parser = new HabrCareerDateTimeParser();
+        HabrCareerParse habrCareerParse = new HabrCareerParse(parser);
+
+        List<Post> posts = new ArrayList<>();
         for (int pageNumber = 1; pageNumber <= HabrCareerParse.MAX_PAGE; pageNumber++) {
             String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
-            Connection connection = Jsoup.connect(fullLink);
-            Document document = connection.get();
-
-            Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                String vacancyName = titleElement.text();
-
-                String date = row.select(".vacancy-card__date")
-                        .first()
-                        .child(0)
-                        .attr("datetime");
-                LocalDateTime time = parser.parse(date);
-
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                String description = retrieveDescription(link);
-                System.out.printf("%s: %s - %s%n", vacancyName, link, time);
-                System.out.printf("%s %n%n", description);
-            });
+            posts.addAll(habrCareerParse.list(fullLink));
         }
+        posts.forEach(System.out::println);
     }
 }
